c9d43cd400d7e95c7a4ee846dfe3d193d5ca40fc
Asher Aber <asher@yoom.com>
2024-05-05 18:47:44 +0300

    start k8s

--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t multi-env-nginx-react:latest .
--- scripts/staging.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --name test -e ENVIRONMENT=staging -d -p 8080:8080 multi-env-nginx-react:latest
-docker logs test
