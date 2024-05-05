4a82ca500d8d3ac9cc5350f2841348d09e0c5b7c
Asher Aber <asher@yoom.com>
2024-05-05 10:42:23 +0300

    need to test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,11 +24,28 @@ function App() {
   }, []);
 
   return (
-    <div className="app">
-      <img src="reach_logo.png" alt="Reach Logo" />
-      <h1>We are running in the {env ? env : 'Unknown'} env</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div className="app">
+        <img src="reach_logo.png" alt="Reach Logo" />
+        <h1>We are running in the {env ? env : 'Unknown'} env</h1>
+      </div>
     </div>
   );
 }
 
 export default App;
+
